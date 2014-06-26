72cc8fcf78d3b7bcbf4272fb38953eae36294fbb
Doug Tyler <dougwt@gmail.com>
2014-06-25 22:18:11 -0700

    Update natsort for Python 3 support

--- natsort/__init__.py
+++ natsort/__init__.py
@@ -1,4 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function, division, unicode_literals, absolute_import
+
 from .natsort import natsort_key, natsorted, index_natsorted
+from ._version import __version__
 
 __all__ = [
            'natsort_key',
